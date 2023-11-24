4aa5a9e618a1a457cdba8de4bf9576f4764dace1
Abbeer Lal Debb <87080838+Abbeer-Lal-Debb@users.noreply.github.com>
2023-11-24 23:16:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,139 @@
+// ========================================
+// creating a Responsive Navbar component
+// ========================================
+
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header");
+const navbar_lists = document.querySelector(".navbar-lists");
+
+const toggleNavbar = () => {
+    // alert("hi");
+    nav_header.classList.toggle("active");
+};
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
+
+const navbar_link = document.querySelectorAll(".navbar-link");
+
+navbar_link.forEach(element => element.addEventListener("click", () => {
+    nav_header.classList.remove("active");
+}));
+
+// ========================================
+// creating a portfolio tabbed component
+// ========================================
+
+const p_btns = document.querySelector(".p-btns");
+const p_btn = document.querySelectorAll(".p-btn");
+const p_img_elem = document.querySelectorAll(".img-overlay");
+
+p_btns.addEventListener('click', (e) => {
+
+    // we will get which child element was clicked
+    const p_btn_clicked = e.target;
+    console.log(p_btn_clicked);
+
+    if (!p_btn_clicked.classList.contains('p-btn')) return;
+    // always remove the classList first then add the class
+
+    p_btn.forEach((curElem) => curElem.classList.remove("p-btn-active"));
+    p_btn_clicked.classList.add("p-btn-active");
+
+    // to find the p-img class number of the images using the btn data attribute number
+    const btn_num = p_btn_clicked.dataset.btnNum;
+    console.log(btn_num);
+
+    const img_active = document.querySelectorAll(`.p-btn--${btn_num}`)
+
+    p_img_elem.forEach((curElem) => curElem.classList.add("p-image-not-active"));
+    img_active.forEach((curElem) => curElem.classList.remove("p-image-not-active"));
+})
+
+// ========================================
+//  how to add media queries in JS
+// ========================================
+
+function myFunction(widthSize) {
+    if (widthSize.matches) {
+        // If media query matches
+        const swiper = new Swiper(".swiper", {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    } else {
+        const swiper = new Swiper(".swiper", {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    }
+}
+
+const widthSize = window.matchMedia("(max-width: 780px)");
+// Call listener function at run time
+myFunction(widthSize);
+// Attach listener function on state changes
+widthSize.addListener(myFunction);
+
+// ========================================
+//  scroll to top Button
+// ========================================
+
+const heroSection = document.querySelector('.header')
+const footerElem = document.querySelector('.section-footer')
+
+const scrollElement = document.createElement('div');
+scrollElement.classList.add('scrollTop-style');
+
+// I am adding the button element inside the div element
+scrollElement.innerHTML = `<ion-icon name="arrow-up-outline" class="scroll-top"></ion-icon>`;
+
+// add to the bottom of the page
+footerElem.after(scrollElement);
+
+// deleting the dom element
+const scrollTop = () => {
+    heroSection.scrollIntoView({ behavior: "smooth" })
+}
+scrollElement.addEventListener('click', scrollTop)
+
+
+// // ========================================
+// //  smooth scrolling
+// // ========================================
+
+const portfolioSection = document.querySelector(".section-portfolio");
+const contactSection = document.querySelector(".section-contact");
+const freelancingSection = document.querySelector(".section-freelancing");
+
+document.querySelector(".portfolio-link").addEventListener("click", (e) => {
+    e.preventDefault();
+    portfolioSection.scrollIntoView({ behavior: "smooth" });
+});
+
+document.querySelector(".hireme-btn").addEventListener("click", (e) => {
+    e.preventDefault();
+    contactSection.scrollIntoView({ behavior: "smooth" });
+});
+
+function resetForm (){
+    let submit = document.getElementById('submit');
+    submit.reset();
+}
+
